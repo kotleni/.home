9b2e5bdf7e10b7c0fd2d944476043cdf5365532e
Viktor Varenik <yavarenikya@gmail.com>
2024-10-15 21:19:37 +0000

    chore(auto): ${key}

--- .src/auto-update-git.sh
+++ .src/auto-update-git.sh
@@ -2,6 +2,13 @@
 
 key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
-git add *
+git add .src
+git add .md
+git add -f .config/nvim/
+git add -f .config/wofi/
+git add -f .config/kitty/
+git add -f .config/hypr/
+git add -f .config/waybar/
+
 git commit -m 'chore(auto): ${key}'
 git push -u origin lgbt-edition
